f551d48c954f97802a781e2ed21b5d29ca0c6864
CORP\aceylan <ahmetcanceylan@gmail.com>
2023-05-22 14:24:31 +0200

    game creation test cases added

---
+++ src/test/java/com/accyln/tictactoe/GameCreationTests.java
@@ -0,0 +1,27 @@
+package com.accyln.tictactoe;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GameCreationTests {
+
+    @Test
+    @DisplayName("Testing that players created correctly")
+    public void assert_that_players_created(){
+
+    }
+
+    @Test
+    @DisplayName("Testing that game board created correctly")
+    public void assert_that_game_board_created(){
+
+    }
+
+    @Test
+    @DisplayName("Testing that game created correctly")
+    public void assert_that_game_created_correctly(){
+
+    }
+}
