35a1ab94cc0b261814d067ca6db4bf31732db899
CORP\aceylan <ahmetcanceylan@gmail.com>
2023-05-25 21:14:08 +0200

    game movement steps added, makeAmove request fetched,

--- src/webui/src/components/Game.js
+++ src/webui/src/components/Game.js
@@ -16,16 +16,94 @@ export function Game(props){
     function getgameInfo() {
         fetch(
           "http://localhost:8080/api/v1/game/getGameById?gameId="+propsVariables.gameId
-        ).then((data) => {
+        ).then(response => response.json())
+        .then((data) => {
           if (data) {
             setGame(data);
           }
         });
       }
 
+    function makeAmove(index,sign){
+        let row=0;
+        let col=0;
+        switch (index) {
+          case 0:
+            row=0;
+            col=0;
+          break;
+          case 1:
+            row=0;
+            col=1;
+          break;
+          case 2:
+            row=0;
+            col=2;
+          break;
+          case 3:
+            row=1;
+            col=0;
+          break;
+          case 4:
+            row=1;
+            col=1;
+          break;
+          case 5:
+            row=1;
+            col=2;
+          break;
+          case 6:
+            row=2;
+            col=0;
+          break;
+          case 7:
+            row=2;
+            col=1;
+          break;
+          case 8:
+            row=2;
+            col=2;
+          break;
+        
+          default:
+            break;
+        }
+
+        let requestBody={
+            gameId: game.id,
+             rowId: row,
+             colId: col,
+             sign: sign
+        }
+
+        var header = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           };
+
+        fetch("http://localhost:8080/api/v1/game/makeAmove", {
+          method: 'POST',
+          headers:header,
+          body: JSON.stringify(requestBody)
+        }).then(response => response.json())
+          .then((data) => {
+            if (data) {
+              setGame(data);
+            }
+          });
+        
+      }
+
+
+    const handleClickSquare = (index) => {
+            makeAmove(index,game?.lastPlayedSign == 'X' ? 'O' : 'X');   
+    }
+    
+
     const renderSquare = (index,sign) => {
         return (
-          <Button className="square" style={{backgroundColor:"whitesmoke",height:70,width:70,borderColor:"gray"}}>
+          <Button className="square" style={{backgroundColor:"whitesmoke",height:70,width:70,borderColor:"gray"}}
+          onClick={()=>handleClickSquare(index)}>
            <span style={{color:"black"}}>{sign}</span>
           </Button>
         );
@@ -40,7 +118,7 @@ export function Game(props){
         <Col sm={4}></Col>
       <Col sm={4}><center>
       <div className="gameBoard">
-        {game ? (<>
+        {game?.board ? (<>
       <div className="board-row">
         {renderSquare(0,game?.board?.[0]?.[0])}
         {renderSquare(1,game?.board?.[0]?.[1])}
